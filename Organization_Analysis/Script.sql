355174290ec837f4378759bfb74933cc808771f6
manpreet-singh-ms <manpreet.singh.maspr@gmail.com>
2023-08-19 20:13:21 +0530

    Queries Documment Formatted

--- Organization_Analysis/Script.sql
+++ Organization_Analysis/Script.sql
@@ -15,14 +15,14 @@ show tables;
 
 desc worker;
 
-insert into worker values(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
-		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
-		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
-		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
-		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
-		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
-		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
-		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+insert into worker values	(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
+							(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
+							(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
+							(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
+							(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
+							(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
+							(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
+							(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
         
 select * from worker;
 
@@ -33,11 +33,11 @@ bonus_date DATETIME,
 foreign key (worker_ref_id) references worker(worker_id)
 on delete cascade);
 
-insert into bonus values (001, 5000, '16-02-20'),
-		(002, 3000, '16-06-11'),
-		(003, 4000, '16-02-20'),
-		(001, 4500, '16-02-20'),
-		(002, 3500, '16-06-11');
+insert into bonus values(001, 5000, '16-02-20'),
+						(002, 3000, '16-06-11'),
+						(003, 4000, '16-02-20'),
+						(001, 4500, '16-02-20'),
+						(002, 3500, '16-06-11');
         
 select * from bonus;
 
@@ -49,13 +49,13 @@ foreign key(worker_ref_id) references worker(worker_id)
 ON DELETE cascade);
 
 insert into title values(001, 'Manager', '2016-02-20 00:00:00'),
- (002, 'Executive', '2016-06-11 00:00:00'),
- (008, 'Executive', '2016-06-11 00:00:00'),
- (005, 'Manager', '2016-06-11 00:00:00'),
- (004, 'Asst. Manager', '2016-06-11 00:00:00'),
- (007, 'Executive', '2016-06-11 00:00:00'),
- (006, 'Lead', '2016-06-11 00:00:00'),
- (003, 'Lead', '2016-06-11 00:00:00');
+						(002, 'Executive', '2016-06-11 00:00:00'),
+						(008, 'Executive', '2016-06-11 00:00:00'),
+						(005, 'Manager', '2016-06-11 00:00:00'),
+						(004, 'Asst. Manager', '2016-06-11 00:00:00'),
+						(007, 'Executive', '2016-06-11 00:00:00'),
+						(006, 'Lead', '2016-06-11 00:00:00'),
+						(003, 'Lead', '2016-06-11 00:00:00');
  
  select * from worker;
  select * from bonus;
